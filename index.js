6ec7bdaa485a9b204e0b0cf445db398425bcc464
LightLord <87213208+LightLordYT@users.noreply.github.com>
2023-01-05 21:50:30 -0800

    closes the google site also forgot to add userscrfipt stuff

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
+// ==UserScript==
+// @name         Canvas Auto Login (for smuhsd)
+// @namespace    https://github.com/LightLordYT
+// @version      1
+// @description  auto opens login also closes that annoying google sites(ew) canvas website
+// @author       LightLord
+// @match        https://smuhsd.instructure.com/login?needs_cookies=1
+// @match        https://sites.google.com/smuhsd.org/canvas
+// @match        https://developer.mozilla.org/en-US/docs/Glossary/Endianness
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=instructure.com
+// @grant window.close
+// ==/UserScript==
+
 let location = window.location.href
 
 if (location == "https://smuhsd.instructure.com/login?needs_cookies=1") {
-	let btn = document.getElementById("global_nav_login_link")
-	btn.click()
-} else {
+	window.open("https://sites.google.com/smuhsd.org/canvas")
+} else if (location == "https://sites.google.com/smuhsd.org/canvas") {
 	let classes = document.getElementsByClassName("XqQF9c")
 	let link = classes[0]
 	link.click()
+	window.close()
 }
